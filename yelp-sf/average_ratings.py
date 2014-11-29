9087fb3aa7b543d8d72cae6b58f23a0f799080ab
Kenneth Lin <kenlin@berkeley.edu>
2014-11-29 12:56:31 -0800

    yelp average ratings per crime

--- yelp-sf/average_ratings.py
+++ yelp-sf/average_ratings.py
@@ -2,6 +2,7 @@
 
 import pymongo
 import numpy as np
+import pandas as pd
 
 def main():
     client = pymongo.MongoClient()
@@ -12,6 +13,9 @@ def main():
         restaurants = crime['restaurants']
         ratings = [r['rating'] for r in restaurants]
         records[crime['incidentNum']] = np.mean(ratings)
+
+    df = pd.DataFrame.from_dict(records, orient='index')
+    df.to_csv('yelp_average_ratings.csv')
 
 if __name__ == '__main__':
     main()
